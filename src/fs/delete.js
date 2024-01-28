14cba0b75c0c5bae93f612da1647cea74852347f
asdqdsa <asdqdsa@protonmail.com>
2024-01-28 20:26:15 +0400

    feat: implementation of delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const src = path.join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fsPromises.unlink(src);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
