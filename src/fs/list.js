8e3d81d6394188314a092e8fa55fcc46149f39b2
asdqdsa <asdqdsa@protonmail.com>
2024-01-28 20:34:41 +0400

    feat: implementation of list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const dir = path.join(import.meta.dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const fileNames = await fsPromises.readdir(dir);
+    console.log(fileNames);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
