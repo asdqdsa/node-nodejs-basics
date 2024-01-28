3900b488c0547713744581a9d94cf5125c9ec6a2
asdqdsa <asdqdsa@protonmail.com>
2024-01-28 19:12:44 +0400

    feat: implement of create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const fullPath = path.join(import.meta.dirname, 'files', 'fresh.txt');
+const DATA = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fsPromises.writeFile(fullPath, DATA, {
+      encoding: 'utf-8',
+      flag: 'wx',
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
