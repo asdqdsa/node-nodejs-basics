20e18e8b987389e7f1c773b45b56cf1c7bbb737a
asdqdsa <asdqdsa@protonmail.com>
2024-01-28 21:51:15 +0400

    feat: implementation of parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const ENV = process.env;
+  const regex = /^RSS_/;
+  const output = [];
+  let formatOutput = '';
+
+  for (let key in ENV) {
+    if (regex.test(key)) {
+      output.push(`${key}=${ENV[key]}`);
+    }
+  }
+
+  formatOutput = output.join('; ').trimEnd();
+  console.log(formatOutput);
 };
 
 parseEnv();
