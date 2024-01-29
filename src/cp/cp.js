6b35cd223b7ac60aec001d47edc96f5f62d53d17
asdqdsa <asdqdsa@protonmail.com>
2024-01-30 00:55:55 +0400

    feat: implementation of spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'child_process';
+import path from 'path';
+
+const target = path.join(import.meta.dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  fork(target, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['ls', '-l', '-a']);
