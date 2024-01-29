d1d88e30844dab55f1223049b6857b284408857e
asdqdsa <asdqdsa@protonmail.com>
2024-01-29 23:54:52 +0400

    feat: implement Worker Threads for worker.js, main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
+const myWorker = path.join(import.meta.dirname, 'worker.js');
+const availableCores = os.availableParallelism();
+const INCREMENT_NUM = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const workers = [...Array(availableCores)].map((val, idx) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(myWorker, { workerData: INCREMENT_NUM + idx });
+      worker.on('message', (msg) => resolve({ status: 'resolved', data: msg }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+  });
+
+  const out = await Promise.all(workers);
+  console.log(out);
 };
 
 await performCalculations();
