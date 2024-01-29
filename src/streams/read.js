54fd842dbb6b934c3b426cc24004969504546431
asdqdsa <asdqdsa@protonmail.com>
2024-01-29 16:49:54 +0400

    feat: implementation of Streams API functions read, wirte, transform

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import path from 'path';
+
+const src = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const stream = createReadStream(src);
+  stream.on('data', (chunk) => process.stdout.write(chunk + '\n'));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'node:fs';
+import path from 'path';
+
+const src = path.join(import.meta.dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  process.stdin.on('data', (input) => {
+    const writeStream = createWriteStream(src, { flags: 'a' });
+    writeStream.write(input);
+    writeStream.end();
+  });
 };
 
 await write();
