d262de20283cc9e5b2985f3c0af73c1e6a4c9e36
asdqdsa <asdqdsa@protonmail.com>
2025-10-26 20:26:29 +0100

    feat: implement file system utilities

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fsPromises from "fs/promises";
+import path from "path";
+
+const oldPath = path.join(import.meta.dirname, "files", "wrongFilename.txt");
+const newPath = path.join(import.meta.dirname, "files", "properFilename.md");
+
 const rename = async () => {
-  // Write your code here
+  try {
+    await fsPromises.rename(oldPath, newPath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
